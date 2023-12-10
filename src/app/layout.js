1ee65b46da9d5edcc700e74c61c552fa4594d3cf
Jasper Gozum <gozumjasper@gmail.com>
2023-12-10 13:59:01 +0800

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <Head>
+        {/* Add your meta tags here */}
+        <meta property="og:image" content="/gaylads.jpg" />
+        {/* Add other meta tags as needed */}
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
